73b52a318d2e7dce1529a2227d85f083fc3a1405
Guilherme Correa Zago <guilherme.zago@pampa.compasso>
2021-03-04 15:04:32 -0300

    troca de maquina

---
+++ src/components/CreateExercise/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateExercise= () => {
+  return (
+    <h1>CreateExercise</h1>
+  );
+}
+
+export default CreateExercise;
---
+++ src/components/ExerciseList/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExerciseList= () => {
+  return (
+    <h1>ExerciseList</h1>
+  );
+}
+
+export default ExerciseList;
